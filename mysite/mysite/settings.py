1c7f01050350b424dbaefe0754a342de67616193
Gurpreet Singh <PreetEnzo1@gmail.com>
2019-04-21 13:03:37 -0700

    removed database settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,15 +81,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'credb',
-        'USER': 'postgres',
-        'PASSWORD': 'gurpreet83',
-        'HOST': 'localhost'
-    }
-}
+
 
 
 # Password validation
